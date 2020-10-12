14a8d188c4d83e6e9c4bca39887432df1fc65a73
mariamdashti-c34 <70545845+mariamdashti-c34@users.noreply.github.com>
2020-10-12 05:16:01 +0300

    done

---
+++ h.js
@@ -0,0 +1,18 @@
+let grade1 = A
+let grade2 = B
+let grade3 = C 
+let grade3 = D
+
+if(grade1 >=90-100){
+    console.log(`A`)
+}
+
+if(grade2 <=80){
+    console.log(`B`)
+}
+if(grade3 <=70){
+    console.log(`C`)
+}
+if(grade4 <=60){
+    console.log(`D`)
+}
